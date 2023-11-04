ac0b8cf6047dcb34d0dfa5fb9afc1717cbf3ece3
vismitha-priyanka <vismithap.btech22@rvu.edu.in>
2023-11-04 17:12:12 +0530

    first

---
+++ gfg2.py
@@ -0,0 +1,12 @@
+"""
+This module performs addition operation on constants a and b.
+"""
+
+A = 2  # Constant representing the value of a
+B = 3  # Constant representing the value of b
+
+# Perform addition operation
+result = A + B
+
+# Print the result
+print(result)
