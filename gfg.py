94bf4c56db013bcff8264251a6a07d66cfc77188
vismithaaa <25vismitha@gmail.com>
2023-11-04 17:19:50 +0530

    first

---
+++ gfg.py
@@ -0,0 +1,13 @@
+"""
+This Python module calculates the sum of two numbers and prints the result.
+"""
+
+# Define two variables and assign values to them
+A = 1
+B = 2
+
+# Calculate the sum of 'A' and 'B'
+RES = A + B
+
+# Print the result
+print(RES)
